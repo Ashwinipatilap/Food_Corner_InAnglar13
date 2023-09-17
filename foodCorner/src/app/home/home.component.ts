355ccfc972c1f762bf8d63b8df691782d3c7b56d
ashwinipatilap <ashwinipatilapk18@gmail.com>
2023-09-17 13:03:34 +0530

    Foods images display on home page

--- foodCorner/src/app/home/home.component.ts
+++ foodCorner/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  foods :string[]=[];
 
-  constructor() { }
+  constructor(private _fs:FoodService) { }
 
   ngOnInit(): void {
+    this.foods = this._fs.getAll();
   }
 
 }
--- foodCorner/src/app/services/food/food.service.ts
+++ foodCorner/src/app/services/food/food.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class FoodService {
 
   constructor() { }
+
+  getAll():string[]{
+    return [
+      '/assets/burgur.avif',
+      '/assets/chickenbolls.jpg',
+      '/assets/frenchfries.jpg',
+      '/assets/pizz1.avif',
+      '/assets/pizza2.jpg',
+      'assets/pizza3.jpg'
+    ]
+  }
 }
